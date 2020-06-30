9af70e92f88c671630e03fd218eff202834eba10
Marco Politti <marcopolitti.br@gmail.com>
2020-06-30 07:45:34 -0300

    Alert, console, variáveis, funções de string, array e dicionário

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//lista de dicionário
+var frutas = [{nome: "maçã", cor:"vermelha"},{nome: "pêra", cor:"amarela"}];
+console.log(frutas);
+
+
+/*
+//dicionário
+var fruta = {nome: "maçã", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+*/
+
+/*
+var lista = ["Maçã","Abacaxi","Uva"];
+console.log(lista);
+
+lista.push("Limão");
+console.log(lista);
+
+console.log(lista[2]);
+lista.pop();
+
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join("|"));
+*/
+
+/*
+var nome = "Marco Politti";
+var idade = 43;
+
+//alert(nome + " tem " + idade + " anos.");
+
+var idade2 = 10;
+
+console.log(nome);
+console.log(idade + idade2);
+
+var frase = "Japão é o melhor time do mundo!";
+console.log(frase);
+
+frase = frase.replace("Japão","Brasil");
+console.log(frase);
+
+frase = frase.toUpperCase();
+console.log(frase);
+*/
